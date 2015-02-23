4ab308c0c462831fff8dd26676820ba9d3f4f624
Jonny Matts <jonoamatts@gmail.com>
2015-02-23 18:49:47 +0000

    Initial applicatoin commit

---
+++ src/main/java/jonnymatts/facerecognition/App.java
@@ -0,0 +1,13 @@
+package jonnymatts.facerecognition;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
